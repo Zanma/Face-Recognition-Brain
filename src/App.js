5c67e060bd06ee63efafc00ea8f815692b93e8fa
Zainuri Mahmud <zainurimahmud32@gmail.com>
2020-04-08 14:36:21 +0700

    add API face detection

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import React, { Component } from 'react';
+import Clarifai from 'clarifai';
 import Navigation from './components/Navigation/Navigation';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
+import FaceRecognation from './components/FaceRecognation/FaceRecognation';
 import Particles from 'react-particles-js';
 import './App.css';
+
+
+
+const app = new Clarifai.App({
+  apiKey: '4f024e19734d4e97992e98bd572c375d'
+ })
 
 const particleOptions = {
   particles: {
@@ -18,6 +26,38 @@ const particleOptions = {
 }
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      input : '',
+      imageUrl : ''
+    }
+  }
+
+  onInputChange = ( event ) => {
+    console.log('check input')
+    this.setState({input: event.target.value })
+  }
+
+  onButtonSubmit = () => {
+    console.log('check submit')
+    this.setState({imageUrl: this.state.input})
+    app.models 
+    .predict(
+    Clarifai.FACE_DETECT_MODEL,
+      // URL
+      this.state.input
+    )
+    .then
+      (
+        function(response) {
+        console.log(response.outputs[0].data.regions[0].region_info.bounding_box);
+      },
+        function(err) {// there was an error
+      }
+    );
+  }
+
   render(){
     return (
       <div className="App">
@@ -25,9 +65,10 @@ class App extends Component {
                 params={particleOptions} />
         <Navigation /> 
         <Rank />
-        <ImageLinkForm />
-          
-          { /*<Logo /> */}
+        <ImageLinkForm 
+          onInputChange = {this.onInputChange} 
+          onButtonSubmit={this.onButtonSubmit}/>
+        <FaceRecognation  imageUrl={this.state.imageUrl} />
       </div>
     );
   }
--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import './ImageLinkForm.css';
 
-const ImageLinkForm = () => {
+const ImageLinkForm = ( { onInputChange, onButtonSubmit } ) => {
     return(
         <div id='form'>
             <p id='intro-text' className='f4 white'>This Magic Brain will detect face in your picture. Get it a try</p>
             <div className='center'>
                 <div className='center pa3 br3 shadow-5'>
-                    <input type='text' className='f4 pa2 w-70 center'/>
-                    <button className='w-30 grow f4 link ph3 pv2 dib white bg-black'>Detect</button>
+                    <input type='text' className='f4 pa2 w-70 center' onChange={onInputChange}/>
+                    <button 
+                    className='w-30 grow f4 link ph3 pv2 dib white bg-black'
+                    onClick={onButtonSubmit}
+                    >
+                        Detect</button>
                 </div>
             </div>
         </div>
